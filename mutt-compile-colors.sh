1d22992f3814f1d7fd7cbeecb11f3fe35d6b8fa1
Ethan Schoonover <es@ethanschoonover.com>
2011-03-12 00:13:34 -0800

    [mail] mutt color compile script tweak

--- mutt-compile-colors.sh
+++ mutt-compile-colors.sh
@@ -14,14 +14,14 @@
 # base1     #8fa2a1 14/4 brcyan   245 #8a8a8a 65 -07 -02 143 162 161 178  12  63
 # base2     #e2e3d9  7/7 white    253 #dadada 90 -02  05 226 227 217  70   5  89
 # base3     #f7f0dd 15/7 brwhite  230 #ffffd7 95  00  10 247 240 221  44  11  97
-# yellow    #b58900  3/3 yellow   136 #af8700 60  10  65 181 137   0  45 100 71
+# yellow    #b58900  3/3 yellow   136 #af8700 60  10  65 181 137   0  45 100  71
 # orange    #cb4b16  9/3 brred    166 #d75f00 50  50  55 203  75  22  18  89  80
 # red       #bd000f  1/1 red      124 #af0000 40  65  50 189   0  15 355 100  74
 # magenta   #c42376  5/5 magenta  125 #af005f 45  65 -05 196  35 118 329  82  77
 # violet    #6c71c4 13/5 brmagenta 61 #5f5faf 50  15 -45 108 113 196 237  45  77
 # blue      #268bd2  4/4 blue      33 #0087ff 55 -10 -45  38 139 210 205  82  82
 # cyan      #2aa198  6/6 cyan      37 #00afaf 60 -35 -05  42 161 152 175  74  63
-# green     #859900  2/2 green     64 #5f8700 60 -20  65 133 153   0  68 100 60
+# green     #859900  2/2 green     64 #5f8700 60 -20  65 133 153   0  68 100  60
 
 cat > palette_dark.tmp <<SETPALETTEDARK
 J_base03        ,color233        ,brightblack     ,
