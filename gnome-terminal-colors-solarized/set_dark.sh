3dd3f3cc2eaa07f5876f18c329cafd575f7faa5e
Matt Brown <matt@mattnworb.com>
2011-04-04 11:02:08 -0400

    allow profile name to be passed as argument; also add setting of use_theme_colors to false

--- gnome-terminal-colors-solarized/set_dark.sh
+++ gnome-terminal-colors-solarized/set_dark.sh
@@ -2,10 +2,18 @@
 
 dir=`dirname $0`
 
+PROFILE="Default"
+if [[ $# -ge 1 ]]; then
+    PROFILE=$1
+fi
+
 # set palette
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette `cat $dir/colors/palette`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/palette `cat $dir/colors/palette`
 
 # set foreground to base0 and background to base03 and highlight color to base1
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color `cat $dir/colors/base03`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color `cat $dir/colors/base0`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color `cat $dir/colors/base1`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/background_color `cat $dir/colors/base03`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/foreground_color `cat $dir/colors/base0`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/bold_color `cat $dir/colors/base1`
+
+# make sure the profile is set to not use theme colors
+gconftool-2 -s -t bool /apps/gnome-terminal/profiles/$PROFILE/use_theme_colors false
--- gnome-terminal-colors-solarized/set_light.sh
+++ gnome-terminal-colors-solarized/set_light.sh
@@ -2,11 +2,19 @@
 
 dir=`dirname $0`
 
+PROFILE="Default"
+if [[ $# -ge 1 ]]; then
+    PROFILE=$1
+fi
+
 # set palette
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette `cat $dir/colors/palette`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/palette `cat $dir/colors/palette`
 
 # set foreground to base00 and background to base3 and highlight color to
 # base01
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color `cat $dir/colors/base3`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color `cat $dir/colors/base00`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color `cat $dir/colors/base01`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/background_color `cat $dir/colors/base3`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/foreground_color `cat $dir/colors/base00`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/bold_color `cat $dir/colors/base01`
+
+# make sure the profile is set to not use theme colors
+gconftool-2 -s -t bool /apps/gnome-terminal/profiles/$PROFILE/use_theme_colors false
